ef2c35cbcc57010f8f16a058566545f70cba9515
Per-Terra <53376589+Per-Terra@users.noreply.github.com>
2023-12-25 22:24:31 +0900

    add Amazonっぽい support and fix a bug

--- tools/lib/Get-FileNameFromUrl.ps1
+++ tools/lib/Get-FileNameFromUrl.ps1
@@ -13,24 +13,33 @@ filter Get-FileNameFromUrl {
       throw "Invalid Url: $_"
     }
 
-    $fileName = if (Split-Path -Path $_ -Extension) {
+    Write-Verbose "Trying to get file name from url: $_"
+
+    $extension = Split-Path -Path $_ -Extension
+    $response = $null
+
+    if ($_ -match 'https://hazumurhythm\.com/php/amazon_download\.php\?name=(.+)') {
+      $id = $Matches[1]
+      $response = Invoke-WebRequest -Uri $_ -Method Head -Headers @{ Referer = "https://hazumurhythm.com/wev/amazon/?script=$id" }
+    }
+    elseif ($extension -and $extension -notmatch '^\.(php)') {
       Write-Verbose "Found file extension in url: $_"
-      Split-Path -Path $_ -Leaf
+      return Split-Path -Path $_ -Leaf
     }
     else {
       Write-Verbose "No file extension found in url: $_"
       Write-Verbose "Trying to get file name from 'Content-Disposition' header..."
       $response = Invoke-WebRequest -Uri $_ -Method Head
-      if ($response.Headers['Content-Disposition']) {
-        if ($response.Headers['Content-Disposition'] -match 'filename="(.+)"') {
-          Write-Verbose "Found file name from 'Content-Disposition' header: $($Matches[1])"
-          $Matches[1]
-        }
-        Write-Verbose "No file name found from 'Content-Disposition' header."
-      }
-      Write-Verbose "No 'Content-Disposition' header found."
     }
 
-    return $fileName
+    # $response.Headers['Content-Disposition']は配列なので最初の要素を取得
+    if ($response.Headers['Content-Disposition'][0]) {
+      if ($response.Headers['Content-Disposition'][0] -match 'filename="(.+)"') {
+        Write-Verbose "Found file name from 'Content-Disposition' header: $($Matches[1])"
+        return $Matches[1]
+      }
+      Write-Verbose "No file name found from 'Content-Disposition' header."
+    }
+    Write-Verbose "No 'Content-Disposition' header found."
   }
 }
