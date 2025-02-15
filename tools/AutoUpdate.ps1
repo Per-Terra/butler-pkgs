c281e5c4d545abf96a7811a0ee3f11352ac24f96
per_terra <53376589+Per-Terra@users.noreply.github.com>
2025-02-15 23:49:30 +0900

    AutoUpdate.ps1の処理を全体的に見直しし最適化

--- tools/AutoUpdate.ps1
+++ tools/AutoUpdate.ps1
@@ -1,156 +1,207 @@
 [CmdletBinding()]
 param (
+  [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)]
+  [Alias('FullName')]
   [string[]]$YamlPath
 )
 
-# powershell-yaml のインストール
-if (-not (Get-Module -Name 'powershell-yaml' -ListAvailable)) {
-  try {
-    Install-Module -Name 'powershell-yaml' -Force -Repository PSGallery -Scope CurrentUser
-  }
-  catch {
-    throw "powershell-yaml のインストールに失敗しました: $($_.Exception.Message)"
-  }
-}
-
-$targets = @()
-
-if ($YamlPath) {
-  $YamlPath | ForEach-Object {
-    Get-Item -LiteralPath $_ | Get-Content -Raw | ConvertFrom-Yaml | ForEach-Object {
-      $targets += $_
+begin {
+  # powershell-yaml のインストール
+  if (-not (Get-Module -Name 'powershell-yaml' -ListAvailable)) {
+    try {
+      Install-Module -Name 'powershell-yaml' -Force -Repository PSGallery -Scope CurrentUser
+    }
+    catch {
+      throw "powershell-yaml のインストールに失敗しました: $($_.Exception.Message)"
     }
   }
-}
-else {
-  $yamls = Get-ChildItem -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath "../auto-update") -Filter '*.yaml' -Recurse -File
-  $yamls | ForEach-Object {
-    $_ | Get-Content -Raw | ConvertFrom-Yaml | ForEach-Object {
-      $targets += $_
-    }
-  }
-}
 
-Write-Host "$($targets.Count) 件の処理を開始します"
+  function Get-GitHubReleases {
+    [CmdletBinding()]
+    param (
+      [Parameter(Mandatory)]
+      [string]$Owner,
+      [Parameter(Mandatory)]
+      [string]$Repo,
+      [ValidateRange(1, 100)]
+      [uint]$PerPage = 30,
+      [uint]$Page = 1,
+      [switch]$Latest
+    )
 
-function Get-GitHubReleases {
-  [CmdletBinding()]
-  param (
-    [Parameter(Mandatory)]
-    [string]$Owner,
-    [Parameter(Mandatory)]
-    [string]$Repo,
-    [ValidateRange(1, 100)]
-    [uint]$PerPage = 100,
-    [uint]$Page = 1,
-    [switch]$Latest
-  )
-
-  # https://docs.github.com/ja/rest/releases/releases?apiVersion=2022-11-28#list-releases
-  $releasesUrl = "https://api.github.com/repos/$Owner/$Repo/releases"
-  if ($Latest) {
-    $releasesUrl += '/latest'
-  }
-  else {
-    $releasesUrl += "?per_page=$PerPage&page=$Page"
-  }
-
-  try {
-    if ($env:GH_TOKEN) {
-      $response = Invoke-RestMethod -Uri $releasesUrl -Authentication Bearer -Token (ConvertTo-SecureString -String $env:GH_TOKEN -AsPlainText -Force)
+    # https://docs.github.com/ja/rest/releases/releases?apiVersion=2022-11-28#list-releases
+    $releasesUrl = "https://api.github.com/repos/$Owner/$Repo/releases"
+    if ($Latest) {
+      $releasesUrl += '/latest'
     }
     else {
-      $response = Invoke-RestMethod -Uri $releasesUrl
+      $releasesUrl += "?per_page=$PerPage&page=$Page"
     }
-  }
-  catch {
-    throw "GitHub API へのリクエストに失敗しました: $($_.Exception.Message)"
-  }
 
-  if ($response) {
-    $response
-  }
-  else {
-    throw "GitHub API からのレスポンスが空です: $releasesUrl"
+    try {
+      if ($env:GH_TOKEN) {
+        $response = Invoke-RestMethod -Uri $releasesUrl -Authentication Bearer -Token (ConvertTo-SecureString -String $env:GH_TOKEN -AsPlainText -Force)
+      }
+      else {
+        $response = Invoke-RestMethod -Uri $releasesUrl
+      }
+    }
+    catch {
+      throw "GitHub API へのリクエストに失敗しました: $($_.Exception.Message)"
+    }
+
+    if ($response) {
+      $response
+    }
+    else {
+      throw "GitHub API からのレスポンスが空です: $releasesUrl"
+    }
   }
 }
 
-foreach ($target in $targets) {
-  if ($target.Enable) {
-    Write-Host "処理中: $($target.Developer)/$($target.Identifier)"
-  }
-  else {
-    Write-Host "無効: $($target.Developer)/$($target.Identifier)"
-    continue
+process {
+  if ([string]::IsNullOrEmpty($YamlPath)) {
+    Write-Host 'auto-update ディレクトリ内の YAML ファイルを読み込んでいます'
+    $YamlPath = Get-ChildItem -LiteralPath (Join-Path -Path $PSScriptRoot -ChildPath "../auto-update") -Filter '*.yaml' -Recurse -File | Select-Object -ExpandProperty FullName
   }
 
-  switch ($target.Type) {
-    'GitHub' {
-      Write-Host "GitHubからリリースを取得しています: $($target.Owner)/$($target.Repository)"
-      $page = 1
-      $skipped = $false
-      do {
-        $releases = Get-GitHubReleases -Owner $target.Owner -Repo $target.Repository -Page $page
-        foreach ($release in $releases) {
-          $asset = $release.assets | Where-Object { $_.name -match $target.Asset } | Select-Object -First 1
-          $url = $asset.browser_download_url
-          if ([string]::IsNullOrEmpty($url)) {
-            Write-Warning "Assetが見つかりません: $($target.Owner)/$($target.Repository)"
-            Write-Warning "Assetの正規表現: $($target.Asset)"
-            Write-Warning "URL: $($release.html_url)"
-            continue
-          }
+  $targets = @()
 
-          $versionFrom = $target.Version.from.Split('.')
-          if ($versionFrom[0] -eq 'asset') {
-            $version = $asset.($versionFrom[1])
-          }
-          else {
-            $version = $release.($versionFrom[0])
-          }
-          if ($target.Version.regex -and $version -match $target.Version.regex.find) {
-            $version = $version -replace $target.Version.regex.find, $target.Version.regex.replace
-          }
-          if ($target.Version.script) {
-            Invoke-Expression -Command $target.Version.script
-          }
-          if ([string]::IsNullOrEmpty($version)) {
-            Write-Warning "バージョンが見つかりません: $($target.Developer)/$($target.Identifier)"
-            Write-Warning "バージョンの取得元: $($target.Version.from)"
-            Write-Warning "バージョンの正規表現: s/$($target.Version.regex.find)/$($target.Version.regex.replace)/"
-            Write-Warning "URL: $($release.html_url)"
-            continue
-          }
+  foreach ($path in $YamlPath) {
+    try {
+      $data = Get-Content -LiteralPath $path -Raw | ConvertFrom-Yaml
+      if ($data) {
+        $targets += $data
+      }
+      else {
+        Write-Warning "YAML ファイルの内容が空です: $path"
+      }
+    }
+    catch {
+      Write-Warning "YAML ファイルの読み込みに失敗しました: $path"
+      Write-Warning "エラー: $($_.Exception.Message)"
+    }
+  }
 
-          $date = $asset.updated_at.ToString('yyyy-MM-dd')
+  Write-Host "$($targets.Count) 件の処理を開始します"
 
-          $manifestPath = Join-Path -Path $PSScriptRoot -ChildPath "../manifests/$($target.Developer)/$($target.Identifier)/$version.yaml"
+  foreach ($target in $targets) {
+    if ($target.Enable) {
+      Write-Host "処理中: $($target.Developer)/$($target.Identifier)"
+    }
+    else {
+      Write-Host "無効: $($target.Developer)/$($target.Identifier)"
+      continue
+    }
 
-          if (Test-Path -LiteralPath $manifestPath -PathType Leaf) {
-            $null = (Get-Content -LiteralPath $manifestPath -Raw) -match "`nReleaseDate: (.*)`n"
-            $releaseDate = $Matches[1]
-            if ($releaseDate -and $releaseDate -ge $date) {
-              Write-Host "該当するバージョンのより新しいマニフェストが既に存在します: $($target.Developer)/$($target.Identifier) ($version)"
-              Write-Debug "作成中のマニフェストのリリース日: $date"
-              Write-Debug "既存のマニフェストのリリース日: $releaseDate"
-              Write-Debug "マニフェストの作成はスキップされました"
-              $skipped = $true
+    $sources = @()
+
+    switch ($target.Type) {
+      'GitHub' {
+        Write-Host "GitHubからリリースを取得しています: $($target.Owner)/$($target.Repository)"
+        $page = 1
+        $skipped = $false
+        do {
+          $releases = Get-GitHubReleases -Owner $target.Owner -Repo $target.Repository -PerPage 30 -Page $page
+          foreach ($release in $releases) {
+            $asset = $release.assets | Where-Object { $_.name -match $target.Asset } | Select-Object -First 1
+            $url = $asset.browser_download_url
+            if ([string]::IsNullOrEmpty($url)) {
+              Write-Warning "Assetが見つかりません: $($target.Owner)/$($target.Repository)"
+              Write-Warning "Assetの正規表現: $($target.Asset)"
+              Write-Warning "URL: $($release.html_url)"
               continue
             }
-          }
 
-          try {
-            . (Join-Path -Path $PSScriptRoot -ChildPath "./CreateManifest.ps1") -Update -SourceUrl $url -Identifier $target.Identifier -Version $version -ReleaseDate $date -Developer $target.Developer -SkipPrompt -Force
+            $versionFrom = $target.Version.from.Split('.')
+            if ($versionFrom[0] -eq 'asset') {
+              $version = $asset.($versionFrom[1])
+            }
+            else {
+              $version = $release.($versionFrom[0])
+            }
+            if ($target.Version.regex -and $version -match $target.Version.regex.find) {
+              $version = $version -replace $target.Version.regex.find, $target.Version.regex.replace
+            }
+            if ($target.Version.script) {
+              Invoke-Expression -Command $target.Version.script
+            }
+            if ([string]::IsNullOrEmpty($version)) {
+              Write-Warning "バージョンが見つかりません: $($target.Developer)/$($target.Identifier)"
+              Write-Warning "バージョンの取得元: $($target.Version.from)"
+              Write-Warning "バージョンの正規表現: s/$($target.Version.regex.find)/$($target.Version.regex.replace)/"
+              Write-Warning "URL: $($release.html_url)"
+              continue
+            }
+
+            $date = $asset.updated_at.ToString('yyyy-MM-dd')
+
+            $manifestPath = Join-Path -Path $PSScriptRoot -ChildPath "../manifests/$($target.Developer)/$($target.Identifier)/$version.yaml"
+
+            if (Test-Path -LiteralPath $manifestPath -PathType Leaf) {
+              $null = (Get-Content -LiteralPath $manifestPath -Raw) -match "`nReleaseDate: (.*)`n"
+              $releaseDate = $Matches[1]
+              if ($releaseDate -and $releaseDate -ge $date) {
+                Write-Host "該当するバージョンのより新しいマニフェストが既に存在します: $($target.Developer)/$($target.Identifier) ($version)"
+                Write-Debug "作成中のマニフェストのリリース日: $date"
+                Write-Debug "既存のマニフェストのリリース日: $releaseDate"
+                Write-Debug "マニフェストの作成はスキップされました"
+                $skipped = $true
+                continue
+              }
+            }
+
+            $sources += @{
+              SourceUrl   = $url
+              Identifier  = $target.Identifier
+              Version     = $version
+              ReleaseDate = $date
+              Developer   = $target.Developer
+            }
           }
-          catch {
-            Write-Warning "マニフェストは作成されませんでした: $($_.Exception.Message)"
+          $page++
+        } while ($releases.Count -eq 30 -and -not $skipped)
+      }
+      default {
+        throw "未対応のタイプです: $($target.Type)"
+      }
+    }
+
+    if ($sources) {
+      Write-Host "$($sources.Count) 件のマニフェストが作成されます: $($target.Developer)/$($target.Identifier)"
+
+      # 古いバージョンから順にマニフェストを作成する
+      $sources = $sources | Sort-Object -Property @{
+        Expression = {
+          if ($null -ne $_.Version) {
+            $_.Version
+          }
+          else {
+            ''
           }
         }
-        $page++
-      } while ($releases.Count -eq 100 -and -not $skipped)
+      }, @{
+        Expression = {
+          if ($null -ne $_.ReleaseDate) {
+            [datetime]::ParseExact($_.ReleaseDate, 'yyyy-MM-dd', $null)
+          }
+          else {
+            [datetime]::MinValue
+          }
+        }
+      }
+
+      foreach ($source in $sources) {
+        try {
+          . (Join-Path -Path $PSScriptRoot -ChildPath "./CreateManifest.ps1") -Update -SourceUrl $source.SourceUrl -Identifier $source.Identifier -Version $source.Version -ReleaseDate $source.ReleaseDate -Developer $source.Developer -SkipPrompt -Force
+        }
+        catch {
+          Write-Warning "マニフェストは作成されませんでした: $($_.Exception.Message)"
+        }
+      }
     }
-    default {
-      throw "未対応のタイプです: $($target.Type)"
+    else {
+      Write-Host "マニフェストは作成されませんでした: $($target.Developer)/$($target.Identifier)"
     }
   }
 }
