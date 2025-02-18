5d24f8c1c2d2c3e11d7923749775186d812942f9
per_terra <53376589+Per-Terra@users.noreply.github.com>
2025-02-18 23:11:29 +0900

    テストを追加

---
+++ tools/TestManifest.ps1
@@ -0,0 +1,68 @@
+[CmdletBinding()]
+param (
+  [Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+  [Alias('FullName')]
+  [string[]]$ManifestPath
+)
+
+begin {
+  Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath './modules/Test-PackageManifest.psm1')
+  $invalidManifests = @()
+}
+
+process {
+  foreach ($path in $ManifestPath) {
+    # Check if the path exists
+    if (-not (Test-Path -Path $path)) {
+      Write-Error "The path '$path' does not exist."
+      $invalidManifests += $path
+      continue
+    }
+
+    # Load the manifest file
+    try {
+      $manifest = Get-Content -LiteralPath $path -Raw | ConvertFrom-Yaml -Ordered
+    }
+    catch {
+      Write-Error "Failed to parse the manifest file '$path'."
+      $invalidManifests += $path
+      continue
+    }
+
+    # Check if the manifest is null or empty
+    if ($null -eq $manifest) {
+      Write-Error "The content of the manifest file '$path' is null or empty."
+      $invalidManifests += $path
+      continue
+    }
+
+    # Check if the manifest is valid
+    Write-Host "Checking manifest file '$path'..."
+    if (-not (Test-PackageManifest -Manifest $manifest)) {
+      Write-Error "The manifest file '$path' is invalid."
+      $invalidManifests += $path
+      continue
+    }
+
+    # Check Filename matches the Version value in the manifest
+    $filename = [System.IO.Path]::GetFileNameWithoutExtension($path)
+    if ($filename -ne $manifest.Version) {
+      Write-Warning "The filename '$filename' does not match the Version value in the manifest."
+      $invalidManifests += $path
+      continue
+    }
+  }
+}
+
+end {
+  if ($invalidManifests) {
+    Write-Error 'The manifest file(s) is/are invalid.'
+    foreach ($invalid in $invalidManifests) {
+      Write-Error "Invalid manifest: $invalid"
+    }
+    exit 1
+  }
+
+  Write-Host -ForegroundColor Green 'The manifest file(s) is/are valid.'
+  exit 0
+}
